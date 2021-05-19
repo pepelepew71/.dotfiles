947d64fa936b30d6cdf1d02eecd497b52f69fe75
yc.huang71 <yc.huang71@gmail.com>
2021-05-19 12:38:20 +0800

    change folder name script to genmon

--- backup.sh
+++ backup.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
+# -- app
 cp ~/.config/redshift.conf ./.config/ -r
 cp ~/.config/autostart ./.config/ -r
 cp ~/.config/powerline-shell ./.config/ -r
-cp ~/.config/scripts ./.config/ -r
 cp ~/.config/rofi ./.config/ -r
-
-cp ~/.config/xfce4/terminal ./.config/xfce4 -r
-
-cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml ./.config/xfce4/xfconf/xfce-perchannel-xml/ -r
-cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ./.config/xfce4/xfconf/xfce-perchannel-xml/ -r
-
 cp ~/.vimrc      ./
 cp ~/.tmux.conf  ./
+cp ~/.bashrc  ./
+
+# -- xfce
+cp ~/.config/genmon ./.config/ -r
+cp ~/.config/xfce4/terminal ./.config/xfce4 -r
+cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml ./.config/xfce4/xfconf/xfce-perchannel-xml/ -r
+cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ./.config/xfce4/xfconf/xfce-perchannel-xml/ -r
