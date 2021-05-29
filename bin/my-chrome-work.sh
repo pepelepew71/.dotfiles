6de1ad635708f134050a76a897b8817b95953a0c
yc.huang71 <yc.huang71@gmail.com>
2021-05-29 23:31:32 +0800

    add i3

---
+++ bin/my-chrome-work.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+google-chrome --new-window --app https://calendar.google.com/calendar/u/0/r https://keep.google.com/u/0/#home https://mail.google.com/mail/u/0/#inbox
---
+++ bin/my-rofi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+rofi \
+    -show drun \
+    -modi "drun,run,ssh" \
+    -show-icons 
