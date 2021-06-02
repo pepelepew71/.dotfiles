caa632f844f4dd81948405b699afb3a30e4a4003
yc.huang71 <yc.huang71@gmail.com>
2021-06-03 00:15:38 +0800

    add ryzen

---
+++ .screenlayout/dual-ryzen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1600x900 --pos 0x0 --rotate right --output DP-0 --primary --mode 1920x1080 --pos 900x260 --rotate normal --output DP-1 --off --output DVI-D-1 --off
