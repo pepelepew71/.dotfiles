a03b74d556204fb9dfac35ff67fb9f5271934382
yc.huang71 <yc.huang71@gmail.com>
2021-06-07 08:02:28 +0800

    mod

---
+++ bin/random-wallpaper.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#nitrogen --set-zoom-fill --random $HOME/wallpapers/twice --head=0 --save
+nitrogen --set-zoom --random $HOME/wallpapers/twice --head=0 --save
+#nitrogen --set-centered --random $HOME/wallpapers/twice --head=0 --save
+#nitrogen --set-auto --random $HOME/wallpapers/twice --head=0 --save
+
