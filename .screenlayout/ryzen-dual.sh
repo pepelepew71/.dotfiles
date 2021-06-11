bc8819398c93cc07374164c125d6619ea789d4a0
yc.huang71 <yc.huang71@gmail.com>
2021-06-12 00:40:57 +0800

    i

--- .screenlayout/ryzen-dual.sh
+++ .screenlayout/ryzen-dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --mode 1600x900 --pos 0x0 --rotate right --output DP-0 --primary --mode 1920x1080 --pos 900x260 --rotate normal
+xrandr --output DP-0 --primary --mode 1920x1080 --pos 900x260 --rotate normal --output HDMI-0 --mode 1600x900 --pos 0x0 --rotate right
