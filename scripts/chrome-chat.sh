3ca68184f7cc9025799687f2812f976778b1e79b
yc.huang71 <yc.huang71@gmail.com>
2022-01-19 23:27:46 +0800

    update

--- scripts/chrome-chat.sh
+++ scripts/chrome-chat.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
-google-chrome-stable --new-window --app https://www.messenger.com/ \
+google-chrome-stable --new-window --app \
+    https://www.facebook.com/messages \
     https://app.slack.com/client \
     chrome-extension://ophjlpahpchlmihnnnihgmmeilfjmjjc/index.html
