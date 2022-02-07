435a83e04dda199bab2ef5f7951ef5f5890bdc44
yc.huang71 <yc.huang71@gmail.com>
2022-02-07 11:44:31 +0800

    add fontconfig and tint2 weather

--- backup.sh
+++ backup.sh
@@ -8,7 +8,9 @@ cp ~/.urxvt/ext/* ./.urxvt/ext
 # -- config
 cp ~/.config/starship.toml ./.config
 cp ~/.config/redshift.conf ./.config
+
 cp ~/.config/picom.conf ./.config
+cp ~/.config/fontconfig/fonts.conf ./.config/fontconfig
 cp ~/.config/conky/conky.conf ./.config/conky
 cp ~/.config/Thunar/uca.xml ./.config/Thunar
 cp ~/.config/i3/config ./.config/i3
@@ -16,6 +18,7 @@ cp ~/.config/pnmixer/config ./.config/pnmixer
 cp ~/.config/i3status/* ./.config/i3status
 cp ~/.config/copyq/copyq.conf ./.config/copyq
 cp ~/.config/tint2/tint2rc ./.config/tint2
+cp ~/.config/tint2/executors ./.config/tint2 -r
 cp ~/.config/rofi ./.config -r
 
 # -- script
